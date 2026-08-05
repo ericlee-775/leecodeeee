971970f00f5a7e2c77441c0ee269586cd0d5c2e4
ericlee-775 <145278946+ericlee-775@users.noreply.github.com>
2026-08-05 15:47:18 +0800

    Time: 2 ms (98.29%), Space: 42.9 MB (96.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
         char[] p = new char[s.length()];
-        HashMap<Character,Character> hmap = new HashMap();
-        hmap.put(')','(');
-        hmap.put('}','{');
-        hmap.put(']','[');
         int index=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==')'||s.charAt(i)==']'||s.charAt(i)=='}'){
-                if(index>0&&p[index-1]==hmap.get(s.charAt(i))){
+                if(index>0&&p[index-1]=='('&&s.charAt(i)==')'){
+                    index--;
+                }
+                else if(index>0&&p[index-1]=='['&&s.charAt(i)==']'){
+                    index--;
+                }
+                else if(index>0&&p[index-1]=='{'&&s.charAt(i)=='}'){
                     index--;
                 }
                 else{
